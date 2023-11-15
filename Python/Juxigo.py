fcb62e318df9f72ff707252faf850085f6cd84db
Felipe, 俳画 <ainfanteh@unal.edu.co>
2023-11-15 08:00:01 -0500

    Delete Python directory

--- Python/Juxigo.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-c = "bcdfghjklmnpqrstvwxyz"
-v = "aeiou"
-for i in range(0, randint(2, 3)):
-    print(c[randint(0, len(c)-1)], end="")
-    print(v[randint(0, len(v)-1)], end="")
